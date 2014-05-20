5f67b2f08eb74e8f5bf1260c1e1727a7f81a8634
Edgar Gressie <edgar@webgar.nl>
2014-05-20 09:29:40 +0200

    Added $flt-from SASS variables, updated JS & style

--- js/form-validate.js
+++ js/form-validate.js
@@ -9,94 +9,94 @@
 
 ;(function ( $, window, undefined ) {
 
-    /**
-     * Form Validator
-     * @constructor
-     * @param {object} element - The HTML element the validator should be bound to
-     * @param {object} options - An option map
-     */
-    function FormValidator(element, options) {
-        this.form = element;
-        this.$form = $(element);
+	/**
+	 * Form Validator
+	 * @constructor
+	 * @param {object} element - The HTML element the validator should be bound to
+	 * @param {object} options - An option map
+	 */
+	function FormValidator(element, options) {
+		this.form = element;
+		this.$form = $(element);
 
-        // Extend the defaults with the passed options
-        this.settings = $.extend( true, {}, FormValidator.defaults, options );
+		// Extend the defaults with the passed options
+		this.settings = $.extend( true, {}, FormValidator.defaults, options );
 
-        // Set the messages
-        this.settings.messages = FormValidator.messages;
+		// Set the messages
+		this.settings.messages = FormValidator.messages;
 
-        // Defined form field array
-        this.elements = [];
+		// Defined form field array
+		this.elements = [];
 
-        // Run the initializer
-        this.init();
-    }
-
-
-    /**
-     * Default plugin settings
-     */
-    FormValidator.defaults = {
-    	messages: {},
-    	groups: {},
-    	rules: {},
-    	errorClass: "error",
-    	validClass: "valid",
-    	errorElement: "label",
-        rowClass: "flt-form__row"
-    };
+		// Run the initializer
+		this.init();
+	}
 
 
-    FormValidator.messages = {
-    	required: "This field is required",
-    	email: "This is not a (correct) email address"
-    };
+	/**
+	 * Default plugin settings
+	 */
+	FormValidator.defaults = {
+		messages: {},
+		groups: {},
+		rules: {},
+		errorClass: "error",
+		validClass: "valid",
+		errorElement: "label",
+		rowClass: "flt-form__row"
+	};
 
 
-    FormValidator.methods = {
-    	/**
-    	 * Checks for empty values
-    	 * @param {string} value 
-    	 */
-    	required: function(value) {
-    		return $.trim(value).length > 0;
-    	}
-    }
+	FormValidator.messages = {
+		required: "This field is required",
+		email: "This is not a (correct) email address"
+	};
 
 
-    // http://jqueryvalidation.org/jQuery.validator.setDefaults/
-    FormValidator.setDefaults = function( settings ) {
-    	$.extend( FormValidator.defaults, settings );
-    };
+	FormValidator.methods = {
+		/**
+		 * Checks for empty values
+		 * @param {string} value 
+		 */
+		required: function(value) {
+			return $.trim(value).length > 0;
+		}
+	}
 
 
-    /**
-     * Initializer
-     */
-    FormValidator.prototype.init = function () {
-
-        //Add novalidate if HTML5
-        this.$form.attr( "novalidate", "novalidate" );
-
-        // Add aria rules for screen readers
-        this.$form.find("[required]").attr("aria-required", "true");
-
-        // Load all fields
-        this.elements = this.findElements();
-
-        // Load events
-        this.loadEvents();
-    };
+	// http://jqueryvalidation.org/jQuery.validator.setDefaults/
+	FormValidator.setDefaults = function( settings ) {
+		$.extend( FormValidator.defaults, settings );
+	};
 
 
-    /**
-     * Inits all the elements
-     */
-    FormValidator.prototype.findElements = function() {
-    	var _this = this;
-    	var foundNames = {};
+	/**
+	 * Initializer
+	 */
+	FormValidator.prototype.init = function () {
 
-    	return  this.$form.find('input, select, textarea')
+		//Add novalidate if HTML5
+		this.$form.attr( "novalidate", "novalidate" );
+
+		// Add aria rules for screen readers
+		this.$form.find("[required]").attr("aria-required", "true");
+
+		// Load all fields
+		this.elements = this.findElements();
+
+		// Load events
+		this.loadEvents();
+	};
+
+
+	/**
+	 * Inits all the elements
+	 */
+	FormValidator.prototype.findElements = function() {
+		var _this = this;
+		var foundNames = {};
+
+		return  this.$form.find('input, select, textarea')
 					.not(":submit, :reset, :image, [disabled]")
 					.filter(function() {
 						// Check if the element has a name, if not, log an error
@@ -113,102 +113,112 @@
 						foundNames[this.name] = $(this);
 						return true;
 					});
-    };
+	};
 
 
-    /**
-     * Inits all the events on the elements
-     */
-    FormValidator.prototype.loadEvents = function() {
-    	var _this = this;
+	/**
+	 * Inits all the events on the elements
+	 */
+	FormValidator.prototype.loadEvents = function() {
+		var _this = this;
 
-        // Form submit
-        this.$form.on('submit', function(e) {
-        	if(_this.settings.debug && window.console) {
-        		console.info('Validating form: ', _this.$form);
-        	}
+		// Form submit
+		this.$form.on('submit', function(e) {
+			if(_this.settings.debug && window.console) {
+				console.info('Validating form: ', _this.$form);
+			}
 
-        	for(var i=0; i < _this.elements.length; i++) {
-        		// $(_this.elements[i]).val(_this.settings.messages.required);
-                var $element = $(_this.elements[i]);
-                var $formRow = $element.closest('.' + _this.settings.rowClass);
-                var $errorElement = $formRow.find('[data-error-placement]');
-                
-                var errorElement = $errorElement.attr('data-error-placement');
-                var formRowError = $formRow.attr('data-error-placement');
+			// remove errors
+			$('.flt-form__error-box').remove()
+			$('.flt-form__fieldset-error').removeClass('flt-form__fieldset-error');
 
-                if (typeof formRowError !== 'undefined' && formRowError !== false ) {
-                    _this.setErrors($formRow, formRowError);
-                } else {
-                    _this.setErrors($errorElement, errorElement);
-                }
-        	}
+			for(var i=0; i < _this.elements.length; i++) {
+				// $(_this.elements[i]).val(_this.settings.messages.required);
+				var $element = $(_this.elements[i]);
+				var $formRow = $element.closest('.' + _this.settings.rowClass);
+				var $errorElement = $formRow.find('[data-error-placement]');
+				
+				var errorElement = $errorElement.attr('data-error-placement');
+				var formRowError = $formRow.attr('data-error-placement');
 
-            return false;
-        });
+				if ($element.hasClass('flt-form__input-validate')) {
 
-    };
+					if (typeof formRowError !== 'undefined' && formRowError !== false ) {
+						_this.setErrors($formRow, formRowError);
+					} else {
+						_this.setErrors($errorElement, errorElement);
+					}
 
+				}
+			}
 
-    FormValidator.prototype.setErrors = function($element, insert) {
-        var _this = this;
-        var $error = $('<p class="flt-form__error-message">' + _this.settings.messages.required + '</p>')
-        
-        switch(insert) {
-            case 'append':
-                $element.append($error);
-                break;
-            case 'after':
-                $element.after($error);
-                break;
-            case 'before':
-                $element.before($error);
-                break;
-            default:
-                $element.prepend($error);
-        }
+			return false;
+		});
 
-    };
+	};
 
 
-    /** jQuery plugin wrapper */
-    $.fn.formValidate = function ( options ) {
-        var args = arguments;
-        // Check the type of the options var
-        if (options === undefined || typeof options === 'object') {
-        	// If no options are passed, or the options var is an object
-        	// it means that this is a 'normal' validator call
-            return this.each(function () {
-            	// Check if this element already has a validator class loaded
-                if (!$.data(this, 'formValidate')) {
-                	// If not, create the class and save it to the element
-                    $.data(this, 'formValidate', new FormValidator( this, options ));
-                }
-            });
-        } else if (typeof options === 'string' && options[0] !== '_' && options !== 'init') {
-        	// If the options var is a string (and it's not a reference to a private or init function)
-        	// it means that this is a call to one of the public functions
-            return this.each(function () {
-            	// Fetch the instance
-                var instance = $.data(this, 'formValidate');
+	FormValidator.prototype.setErrors = function($element, insert) {
+		var _this = this;
+		var $error = $('<div class="flt-form__error-box"><p class="flt-form__error-message">' + _this.settings.messages.required + '</p></div>');
 
-                // Check if this is a correct instance and if the function exists
-                if (instance instanceof FormValidator && typeof instance[options] === 'function') {
-                	// If so, call that function
-                    instance[options].apply( instance, Array.prototype.slice.call( args, 1 ) );
-                }
+		$element.closest('.flt-form__fieldset').addClass('flt-form__fieldset-error');
+		
+		switch(insert) {
+			case 'append':
+				$element.append($error);
+				break;
+			case 'after':
+				$element.after($error);
+				break;
+			case 'before':
+				$element.before($error);
+				break;
+			default:
+				$element.prepend($error);
+		}
 
-                // Allow instances to be destroyed via the 'destroy' method
-                if (options === 'destroy') {
-                    // TODO: destroy instance classes, etc
-                    $.data(this, 'formValidate', null);
-                }
-            });
-        }
-    };
+	};
 
 
-    /** Also publish the class in the $ namespace */
-    $.validator = FormValidator;
+	/** jQuery plugin wrapper */
+	$.fn.formValidate = function ( options ) {
+		var args = arguments;
+		// Check the type of the options var
+		if (options === undefined || typeof options === 'object') {
+			// If no options are passed, or the options var is an object
+			// it means that this is a 'normal' validator call
+			return this.each(function () {
+				// Check if this element already has a validator class loaded
+				if (!$.data(this, 'formValidate')) {
+					// If not, create the class and save it to the element
+					$.data(this, 'formValidate', new FormValidator( this, options ));
+				}
+			});
+		} else if (typeof options === 'string' && options[0] !== '_' && options !== 'init') {
+			// If the options var is a string (and it's not a reference to a private or init function)
+			// it means that this is a call to one of the public functions
+			return this.each(function () {
+				// Fetch the instance
+				var instance = $.data(this, 'formValidate');
+
+				// Check if this is a correct instance and if the function exists
+				if (instance instanceof FormValidator && typeof instance[options] === 'function') {
+					// If so, call that function
+					instance[options].apply( instance, Array.prototype.slice.call( args, 1 ) );
+				}
+
+				// Allow instances to be destroyed via the 'destroy' method
+				if (options === 'destroy') {
+					// TODO: destroy instance classes, etc
+					$.data(this, 'formValidate', null);
+				}
+			});
+		}
+	};
+
+
+	/** Also publish the class in the $ namespace */
+	$.validator = FormValidator;
 
 }(jQuery, window));
